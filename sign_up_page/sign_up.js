2bcb39ca5816254171a98ac77f3b04d8ad11b30d
teamNUM48 <104519108+teamNUM48@users.noreply.github.com>
2022-05-04 17:36:27 +0200

    Add files via upload

--- sign_up_page/sign_up.js
+++ sign_up_page/sign_up.js
@@ -1,22 +1,26 @@
-let data = {
+let login = [{
     username:"",
     email:"",
     password:"",
     fullName:""
-}
+}]
+let x = 1;
 let username = document.getElementById('username')
 let fullName = document.getElementById('fullName')
 let email = document.getElementById('email')
 let password = document.getElementById('pass')
-document.getElementById('register').onsubmit = ()=>{
-    if(username.value != ''){
-    localStorage.login.username = JSON.stringify(username.value)
-    localStorage.login.fullName = JSON.stringify(fullName.value)
-    localStorage.login.email = JSON.stringify(email.value)
-    localStorage.login.password = JSON.stringify(pass.value)
-    return true;
+let register = document.getElementById("register")
+
+register.onsubmit = ()=>{
+    if(x == 1){
+    login[0].username = JSON.stringify(username.value)
+    login[0].fullName = JSON.stringify(fullName.value)
+    login[0].email = JSON.stringify(email.value)
+    login[0].password = JSON.stringify(pass.value)
+    localStorage.login = JSON.stringify(login[0])
+    return true;    
     }
     else{
-        return false;
+        return false
     }
 }
