4808465ec2a763bb25fe1ae8eecebb8ffda32311
teamNUM48 <104519108+teamNUM48@users.noreply.github.com>
2022-05-06 00:04:23 +0200

    Add files via upload

---
+++ player_info_page/player_info.js
@@ -0,0 +1,8 @@
+// side bar
+let btn = document.querySelector("#btn");
+let sidebar = document.querySelector(".sidebar");
+
+btn.onclick = function () {
+    sidebar.classList.toggle("active");
+};
+// --------------------------------
