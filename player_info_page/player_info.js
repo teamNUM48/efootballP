bce64325b6c5a9ec8269d6f43dafa42d1e80b80f
teamNUM48 <104519108+teamNUM48@users.noreply.github.com>
2022-05-08 02:10:06 +0200

    Update player_info.js

--- player_info_page/player_info.js
+++ player_info_page/player_info.js
@@ -6,3 +6,23 @@ btn.onclick = function () {
     sidebar.classList.toggle("active");
 };
 // --------------------------------
+let playerName = document.getElementById(`playerName`);
+let playerTeam = document.getElementById("player-team");
+let playerImg = document.getElementById(`playerImg`);
+let playerPosition = document.getElementById(`player_position`);
+let goals = document.getElementById(`goals`);
+let wins = document.getElementById(`wins`);
+let assists = document.getElementById(`assists`);
+let matches = document.getElementById(`matches`);
+let tittle = document.getElementById('tittle')
+let playerInfo = JSON.parse(localStorage.player);
+
+playerTeam.innerHTML = playerInfo.player_team;
+playerName.innerHTML = playerInfo.player_name;
+playerImg.src = playerInfo.player_img;
+playerPosition.innerHTML = playerInfo.player_position;
+goals.innerHTML = playerInfo.goals;
+wins.innerHTML = playerInfo.wins;
+assists.innerHTML = playerInfo.assists;
+matches.innerHTML = playerInfo.matches;
+tittle.innerHTML = playerInfo.tittle;
