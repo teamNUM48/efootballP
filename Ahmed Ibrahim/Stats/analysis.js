d8cf7f2a3b0f2aca64f038ab1ab8d3584b9323b8
teamNUM48 <104519108+teamNUM48@users.noreply.github.com>
2022-05-09 10:30:33 +0200

    Add files via upload

--- Ahmed Ibrahim/Stats/analysis.js
+++ Ahmed Ibrahim/Stats/analysis.js
@@ -4,3 +4,15 @@ let sidebar = document.querySelector(".sidebar");
 btn.onclick = function () {
     sidebar.classList.toggle("active");
 };
+
+function myfunction(){
+    if(document.querySelector("#man").style.display=="block")
+    {
+    document.querySelector("#man").style.display="none";
+    document.querySelector("#chelsea").style.display="block";
+    }
+else{
+    document.querySelector("#man").style.display="block";
+    document.querySelector("#chelsea").style.display="none";
+}
+}
