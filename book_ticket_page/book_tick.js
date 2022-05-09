ae941205277f331b8f12a17f7c1b93f8e96bd30e
teamNUM48 <104519108+teamNUM48@users.noreply.github.com>
2022-05-09 15:11:11 +0200

    Update book_tick.js

--- book_ticket_page/book_tick.js
+++ book_ticket_page/book_tick.js
@@ -6,3 +6,8 @@ btn.onclick = function () {
     sidebar.classList.toggle("active");
 };
 // --------------------------------
+let booked = document.getElementById("booked");
+let book = document.getElementById("book");
+book.onclick = () => {
+  booked.style.display = "block";
+};
